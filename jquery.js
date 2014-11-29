acd0dcca70e9b7ef7c899f53abd25f34cc714a32
BradenRupp <you@example.com>
2014-11-29 17:12:49 -0600

    lots of changes

--- jquery.js
+++ jquery.js
@@ -21,3 +21,26 @@ $(document).ready(function(){
 		}
 	});
 });
+
+    function stext() {
+        var x = document.getElementById("seeMore1");
+        var y = document.getElementById("seeMore4");
+        x.style.color = '#f0f0f0';
+        x.style.background = '#333';
+
+        y.style.color = '#f0f0f0';
+        y.style.background = '#333';
+    }
+
+    function htext(td) {
+        var x = document.getElementById("seeMore1");
+        var y = document.getElementById("seeMore4");
+        x.style.color = '#333';
+        x.style.background = '#f0f0f0';
+
+        y.style.color = '#333';
+        y.style.background = '#f0f0f0';
+    }
+
+
+
