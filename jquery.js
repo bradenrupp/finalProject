c1b6db4e85a748d792b750fcaca9e8bc17e11c2a
BradenRupp <you@example.com>
2014-11-30 19:17:24 -0600

    changes

--- jquery.js
+++ jquery.js
@@ -1,20 +1,16 @@
 $(document).ready(function(){
-	alert("Welcome");
-
 	var scroll_start = 0;
 	var startchange = $('#startchange');
 	var offset = startchange.offset();
 	$(document).scroll(function() {
 		scroll_start = $(this).scrollTop();
 		if(scroll_start > offset.top) {
-			$('#navbar').css('background-color','#f0f0f0');
-			$('#navbar').css('color','black');
+			$('#navbar').css('background-color','#333');
 			$('#navbar').css('opacity','.9');
 
 		}
 		else {
 			$('#navbar').css('background-color','transparent');
-			$('#navbar').css('color','white');
 			$('#navbar').css('opacity','1');
 
 
@@ -22,25 +18,7 @@ $(document).ready(function(){
 	});
 });
 
-    function stext() {
-        var x = document.getElementById("seeMore1");
-        var y = document.getElementById("seeMore4");
-        x.style.color = '#f0f0f0';
-        x.style.background = '#333';
 
-        y.style.color = '#f0f0f0';
-        y.style.background = '#333';
-    }
-
-    function htext(td) {
-        var x = document.getElementById("seeMore1");
-        var y = document.getElementById("seeMore4");
-        x.style.color = '#333';
-        x.style.background = '#f0f0f0';
-
-        y.style.color = '#333';
-        y.style.background = '#f0f0f0';
-    }
 
 
 
